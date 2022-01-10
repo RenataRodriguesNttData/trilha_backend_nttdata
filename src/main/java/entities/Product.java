502443ea2ddcd340d3f7378557fbe53fe5cba4cd
RenataRodriguesNttData <96358504+RenataRodriguesNttData@users.noreply.github.com>
2022-01-10 13:41:59 -0300

    Atualização

---
+++ src/main/java/entities/Product.java
@@ -0,0 +1,64 @@
+package entities;
+
+public class Product {
+
+    long id;
+    String name;
+    String description;
+    double price;
+
+    public Product() {
+
+    }
+
+    public Product(long id, String n, String d, double p) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
