f0ac537136e875f585219d7a447a5d5790c43678
RenataRodriguesNttData <96358504+RenataRodriguesNttData@users.noreply.github.com>
2022-01-22 00:24:28 -0300

    Aguardando correção

---
+++ financys/src/main/java/br/com/financys/controller/EntryController.java
@@ -0,0 +1,68 @@
+package br.com.financys.controller;
+
+
+import br.com.financys.entities.Entry;
+import br.com.financys.service.EntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/entries")
+public class EntryController {
+
+    @Autowired
+    private EntryService entryService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<Entry> create(@RequestBody Entry entry) throws Exception {
+        if (entryService.validateCategoryById(entry)){
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(entry.getId()).toUri();
+            return ResponseEntity.created(uri).body(entry);
+        }
+            throw new Exception("Id not exist");
+        }
+
+    @GetMapping
+    public ResponseEntity<List<Entry>> findAll(){
+        List<Entry> list = entryService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Entry> findById(@PathVariable Long Id){
+        Entry validateCategoryById = entryService.findById(Id);
+        return ResponseEntity.ok().body(validateCategoryById);
+    }
+
+
+    @PutMapping(value = "/update/{id}")
+    public ResponseEntity<Entry> update(@PathVariable("id") Long id, @RequestBody Entry entry) {
+        Entry update = entryService.update(id, entry);
+        return ResponseEntity.ok().body(update);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long Id){
+        entryService.delete(Id);
+        return ResponseEntity.noContent().build();
+    }
+
+    private Exception newConstraintException(String id_not_exist) {
+        return null;
+    }
+
+
+}
+
+
+
